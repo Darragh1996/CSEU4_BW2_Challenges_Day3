c8e448858d14ac1d31cd2744b367dd12c60523ad
Darragh Ferry <darraghferry@hotmail.com>
2020-05-29 16:23:37 +0100

    day3

--- decode_string/decode_string.py
+++ decode_string/decode_string.py
@@ -1,12 +1,6 @@
-import re
+# 3[a]2[bc]
 
 
 class Solution:
     def decodeString(self, s: str) -> str:
-        new_array = re.findall("\[(.*?)\]", s)
-        print(new_array)
-
-
-# tests
-test = Solution()
-test.decodeString("3[a2[c]]")
+        pass
